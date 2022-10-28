8d2a1c1ef8adea12df235c67684f0cfb92ffe896
takiko22 <ootaki200109@gmail.com>
2022-10-28 09:35:57 +0900

    tmp: リンクコンポーネントの作成

---
+++ pages/posts/firstPost.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const firstPost = () => {
+  return (
+    <>
+      <h1>Hello Next!!</h1>
+      <Link href="/">back to home</Link>
+    </>
+  );
+};
+
+export default firstPost;
