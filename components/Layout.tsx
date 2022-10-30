ac45cf0f2eb52fddf236d056b0583bab9b94a659
takiko22 <ootaki200109@gmail.com>
2022-10-31 08:39:48 +0900

    feat: 雛形作成

---
+++ components/Layout.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+
+const Layout = (children: any) => {
+  const name: string = "shutootaki";
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img
+          src="/images/IMG_3469.jpg"
+          alt="profile"
+          className={styles.Image}
+        />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
--- pages/posts/firstPost.tsx
+++ pages/posts/firstPost.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const firstPost = () => {
   return (
     <>
+      <Head>
+        <title>Posts</title>
+      </Head>
       <h1>Hello Next!!</h1>
       <Link href="/">back to home</Link>
     </>
