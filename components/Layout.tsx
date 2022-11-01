20b65cad9b881ea8b6982f3f6c1fa423a8a21a32
takiko22 <ootaki200109@gmail.com>
2022-11-01 13:08:49 +0900

    chore: imageのサイズ変更

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 const Layout = ({ children, home }: any) => {
   const name: string = "shutootaki";
@@ -12,14 +13,32 @@ const Layout = ({ children, home }: any) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img
-          src="/images/IMG_3469.jpg"
-          alt="profile"
-          className={styles.Image}
-        />
-        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+        {home ? (
+          <>
+            <img
+              src="/images/IMG_3469.jpg"
+              alt="profile"
+              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <img
+              src="/images/IMG_3469.jpg"
+              alt="profile"
+              className={styles.Image}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
       </header>
       <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">⇐ ホームに戻る</Link>
+        </div>
+      )}
     </div>
   );
 };
