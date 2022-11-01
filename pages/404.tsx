167da2f65dc4ecd9f0c2b1def865e151578c30b8
takiko22 <ootaki200109@gmail.com>
2022-11-01 13:08:27 +0900

    feat: 404ページ作成

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+import utilStyles from "../styles/utils.module.css";
+
+const Custom404 = () => {
+  return <div className={utilStyles.headingX1}>page not found</div>;
+};
+
+export default Custom404;
